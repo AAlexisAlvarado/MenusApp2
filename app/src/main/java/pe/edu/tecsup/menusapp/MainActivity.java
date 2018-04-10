0757853bb9e023e0c818f6e02e9ed3c51fdca634
Linder Hassinger <linderhassinger00@gmail.com>
2018-04-10 09:45:17 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/tecsup/menusapp/MainActivity.java
@@ -0,0 +1,89 @@
+package pe.edu.tecsup.menusapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_home:
+                Toast.makeText(this, "Regresando al inicio...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_takepicture:
+                Toast.makeText(this, "Tomando una foto...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_day_view:
+                item.setChecked(true);
+                Toast.makeText(this, "Vista diaria activada...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_week_view:
+                item.setChecked(true);
+                Toast.makeText(this, "Vista semanal activada...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_month_view:
+                item.setChecked(true);
+                Toast.makeText(this, "Vista mensual activada...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_toggle:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    Toast.makeText(this, "Modo offline desactivado...", Toast.LENGTH_SHORT).show();
+                }else{
+                    item.setChecked(true);
+                    Toast.makeText(this, "Modo offline activado...", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case R.id.action_about:
+                Toast.makeText(this, "Acerca de...", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showPopup(View view){
+        PopupMenu popup = new PopupMenu(this, view);
+        // Inflate the menu from xml
+        popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+        // Setup menu item selection
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_favority:
+                        Toast.makeText(MainActivity.this, "La nota ha sido guardada", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu_share:
+                        Toast.makeText(MainActivity.this, "La nota ha sido compartida", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        // Show the menu
+        popup.show();
+    }
+
+
+}
